3312d15e68a959c1f8f6223670cda04b6c1e07c2
Mikhail Ilyushin <mishamine26@mail.ru>
2020-10-05 09:18:13 +0300

    5 лаба.

--- KPiYAP_labs/Program.cs
+++ KPiYAP_labs/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace KPiYAP_labs
@@ -10,6 +11,114 @@ namespace KPiYAP_labs
     {
         static void Main(string[] args)
         {
+            MissionImpossible();
+            Console.ReadLine();
+        }
+
+        private static void StarWars()
+        {
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(250, 500);
+            Thread.Sleep(50);
+            Console.Beep(350, 250);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+            Console.Beep(250, 500);
+            Thread.Sleep(50);
+            Console.Beep(350, 250);
+            Console.Beep(300, 500);
+            Thread.Sleep(50);
+        }
+
+        private static void MissionImpossible()
+        {
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(932, 150);
+            Thread.Sleep(150);
+            Console.Beep(1047, 150);
+            Thread.Sleep(150);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(784, 150);
+            Thread.Sleep(300);
+            Console.Beep(699, 150);
+            Thread.Sleep(150);
+            Console.Beep(740, 150);
+            Thread.Sleep(150);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(587, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(554, 1200);
+            Thread.Sleep(75);
+            Console.Beep(932, 150);
+            Console.Beep(784, 150);
+            Console.Beep(523, 1200);
+            Thread.Sleep(150);
+            Console.Beep(466, 150);
+            Console.Beep(523, 150);
+        }
+
+        private static void Grasshoper()
+        {
+            Console.Beep(440, 300);
+            Console.Beep(330, 300);
+            Console.Beep(440, 300);
+            Console.Beep(330, 300);
+            Console.Beep(440, 300);
+            Console.Beep(415, 300);
+            Console.Beep(415, 300);
+            Thread.Sleep(600);
+            Console.Beep(415, 300);
+            Console.Beep(330, 300);
+            Console.Beep(415, 300);
+            Console.Beep(330, 300);
+            Console.Beep(415, 300);
+            Console.Beep(440, 300);
+            Console.Beep(440, 300);
+            Thread.Sleep(600);
+            Console.Beep(440, 300);
+            Console.Beep(494, 300);
+            Console.Beep(494, 100);
+            Console.Beep(494, 100);
+            Console.Beep(494, 300);
+            Console.Beep(494, 300);
+            Console.Beep(523, 300);
+            Console.Beep(523, 100);
+            Console.Beep(523, 100);
+            Console.Beep(523, 300);
+            Console.Beep(523, 300);
+            Console.Beep(523, 300);
+            Console.Beep(494, 300);
+            Console.Beep(440, 300);
+            Console.Beep(415, 300);
+            Console.Beep(440, 800);
         }
     }
 }
