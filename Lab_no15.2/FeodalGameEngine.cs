941036fe060d94d53dc0e0787f588f23c6bcc66e
Mikhail Ilyushin <mishamine26@mail.ru>
2021-01-15 12:56:23 +0300

    15 15.2

---
+++ Lab_no15.2/FeodalGameEngine.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+
+namespace Lab_no15._2
+{
+    public class FeodalGameEngine
+    {
+	    private static class GameFields
+	    {
+		    public static Random Random = new Random();
+		    public static double PeasentSpawnChance = 0.5;
+		    public static int MaxPeasentCount = -1;
+	    }
+
+	    public event EventHandler LostGame;
+	    public event EventHandler WinGame;
+	    public event EventHandler MoneyEarned;
+	    public event EventHandler MoneySpended;
+	    public event EventHandler PeasentAdded;
+	    public event EventHandler PeasentRemoved;
+
+	    public bool IsGameOn { get; private set; }
+	    private int _peasantsCount;
+	    private int _money = 10;
+
+	    public int PeasantsTargetCount { get; }
+
+	    public int Money
+	    {
+		    get => _money;
+		    private set
+		    {
+			    if(IsGameOn)
+			    {
+				    bool addOrRemove = _money < value;
+				    _money = value;
+					if (addOrRemove)
+					    MoneyEarned?.Invoke(this, EventArgs.Empty);
+				    else
+					    MoneySpended?.Invoke(this, EventArgs.Empty);
+				    if (value < 0)
+					    LostGame?.Invoke(this, EventArgs.Empty);
+				}
+		    }
+	    }
+
+	    public int PeasantsCount
+		{
+			get => _peasantsCount;
+			private set
+			{
+				if(IsGameOn)
+				{
+					bool addOrRemove = _peasantsCount < value;
+					_peasantsCount = value;
+					if (addOrRemove)
+						PeasentAdded?.Invoke(this, EventArgs.Empty);
+                    else
+						PeasentRemoved?.Invoke(this, EventArgs.Empty);
+					if (value == PeasantsTargetCount)
+						WinGame?.Invoke(this, EventArgs.Empty);
+				}
+			}
+		}
+
+		public FeodalGameEngine(int peasantsTargetCount, int startPeasantsCount)
+	    {
+		    PeasantsTargetCount = peasantsTargetCount;
+		    _peasantsCount = startPeasantsCount;
+		    LostGame += ReloadGame;
+		    WinGame += ReloadGame;
+		    IsGameOn = true;
+	    }
+
+	    public void MakeStep(Queue<FeodalActions> actions)
+	    {
+		    while(actions.Any())
+		    {
+				if (!IsGameOn) break;
+				var action = actions.Dequeue();
+				switch (action)
+				{
+					case FeodalActions.IncreaseTax:
+						IncreaseTax();
+						break;
+
+					case FeodalActions.ReduceTax:
+						ReduceTax();
+						break;
+
+					case FeodalActions.BuildShack:
+						BuildShack();
+						break;
+
+					case FeodalActions.GiveFreeRein:
+						GiveFreeRein();
+						break;
+
+					case FeodalActions.HoldCelebration:
+						HoldCelebration();
+						break;
+
+					default:
+						throw new ArgumentOutOfRangeException();
+				}
+			}
+		    
+		    TryToGeneratePeasent();
+	    }
+
+	    private void TryToGeneratePeasent()
+	    {
+		    var randomNumber = GameFields.Random.NextDouble();
+		    if(randomNumber <= GameFields.PeasentSpawnChance)
+			    PeasantsCount++;
+	    }
+
+	    private void ReloadGame(object sender, EventArgs args)
+	    {
+		    IsGameOn = false;
+		    GameFields.PeasentSpawnChance = 0.05;
+		    GameFields.MaxPeasentCount = -1;
+		    _money = -1;
+		    _peasantsCount = -1;
+	    }
+
+		private void IncreaseTax()
+		{
+			Money = Money + 1;
+			GameFields.PeasentSpawnChance -= 0.002;
+		}
+
+	    private void ReduceTax()
+	    {
+		    Money = Money - 1;
+		    GameFields.PeasentSpawnChance += 0.002;
+	    }
+
+	    private void BuildShack()
+	    {
+		    Money -= 10;
+		    GameFields.MaxPeasentCount += 4;
+	    }
+
+	    private void GiveFreeRein()
+	    {
+		    PeasantsCount--;
+		    GameFields.PeasentSpawnChance += 0.05;
+	    }
+
+	    private void HoldCelebration()
+	    {
+		    Money -= 20;
+		    GameFields.PeasentSpawnChance += 0.01;
+	    }
+    }
+
+    public enum FeodalActions
+    {
+	    IncreaseTax,
+	    ReduceTax,
+	    BuildShack,
+	    GiveFreeRein,
+	    HoldCelebration
+	}
+}
---
+++ Lab_no15.2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Lab_no15._2
+{
+    class Program
+    {
+	    static void Main(string[] args)
+        {
+	        Console.WriteLine("Какое количество крестьян Вас устроит, милорд?");
+	        var targetCount = int.Parse(Console.ReadLine());
+	        Console.WriteLine("Какое количество крестьян будет у вас во владении с начала, милорд?");
+	        var startCount = int.Parse(Console.ReadLine());
+	        var game = new FeodalGameEngine(targetCount, startCount);
+	        game.PeasentAdded += AddPeasantEventHandler;
+	        game.PeasentRemoved += RemovePeasantEventHandler;
+	        game.LostGame += LostTheGameEventHandler;
+	        game.WinGame += WonTheGameEventHandler;
+	        game.MoneyEarned += MoneyBalanceChangedEventHandler;
+	        game.MoneySpended += MoneyBalanceChangedEventHandler;
+	        StartGame(game);
+	        Console.ReadLine();
+        }
+        
+        static void AddPeasantEventHandler(object sender, EventArgs args)
+        {
+	        if(sender is FeodalGameEngine engine)
+		        Console.WriteLine($"К нам поступление крестьянина! {engine.PeasantsCount} / {engine.PeasantsTargetCount}");
+        }
+
+        static void RemovePeasantEventHandler(object sender, EventArgs args)
+        {
+	        if (sender is FeodalGameEngine engine)
+		        Console.WriteLine($"Плохие новости! Крестьянин ушёл! {engine.PeasantsCount} / {engine.PeasantsTargetCount}");
+        }
+
+		static void WonTheGameEventHandler(object sender, EventArgs args)
+		{
+			Console.WriteLine("Поздравляю! Вы выиграли");
+		}
+
+		static void LostTheGameEventHandler(object sender, EventArgs args)
+		{
+			Console.WriteLine("К сожалению вы проиграли!");
+		}
+
+		static void MoneyBalanceChangedEventHandler(object sender, EventArgs args)
+		{
+			if (sender is FeodalGameEngine engine)
+				Console.WriteLine($"Ваша казна поменяла баланс! Баланс: {engine.Money}");
+		}
+
+		static Queue<FeodalActions> GetActionsAtStep()
+		{
+			Console.ReadKey(false);
+	        var queue = new Queue<FeodalActions>();
+	        Console.WriteLine("Веберите Ваши действия для следующего шага: ");
+	        bool isEndOfStep = false;
+	        while(!isEndOfStep)
+	        {
+		        Console.WriteLine("1. Увеличить налог");
+		        Console.WriteLine("2. Уменьшить налог");
+		        Console.WriteLine("3. Построить хижину");
+		        Console.WriteLine("4. Дать крестьянину свободу");
+		        Console.WriteLine("5. Провести пати-хард");
+		        Console.WriteLine("6. Закончить действия на этот шаг");
+		        var input = int.Parse(Console.ReadLine().Trim());
+		        switch(input)
+		        {
+					case 1:
+						queue.Enqueue(FeodalActions.IncreaseTax);
+						break;
+					case 2:
+						queue.Enqueue(FeodalActions.ReduceTax);
+						break;
+					case 3:
+						queue.Enqueue(FeodalActions.BuildShack);
+						break;
+					case 4:
+						queue.Enqueue(FeodalActions.GiveFreeRein);
+						break;
+					case 5:
+						queue.Enqueue(FeodalActions.HoldCelebration);
+						break;
+					case 6:
+						isEndOfStep = true;
+						break;
+					default:
+						throw new ArgumentException("Ввёл не то, брат");
+		        }
+	        }
+
+	        return queue;
+        }
+
+        static void StartGame(FeodalGameEngine gameObject)
+        {
+	        while(gameObject.IsGameOn)
+	        {
+		        var actions = GetActionsAtStep();
+		        gameObject.MakeStep(actions);
+	        }
+
+	        Process.GetCurrentProcess().CloseMainWindow();
+        }
+    }
+}
