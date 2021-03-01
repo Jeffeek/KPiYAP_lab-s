3d1972194a2b23681c31cc6c5217f8af9ac58f7f
Mikhail Ilyushin <mishamine26@mail.ru>
2021-03-01 15:34:11 +0300

    20

---
+++ Lab_no20/MathBicycle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_no20
+{
+    public static class MathBicycle
+    {
+        public static async Task<double> Pow(double number, double power)
+        {
+            return await Task.Run(() =>
+                                  {
+                                      var result = number;
+
+                                      while (power > 0)
+                                      {
+                                          result *= number;
+                                          power--;
+                                      }
+
+                                      return result;
+                                  });
+        }
+    }
+}
---
+++ Lab_no20/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab_no20
+{
+    class Program
+    {
+        public static double Power(double num, int power)
+        {
+            var result = num;
+
+            while (power > 0)
+            {
+                result *= num;
+                power--;
+            }
+
+            return result;
+        }
+
+        public static void Shit(int x, int delta)
+        {
+            double power = 1;
+            var i = 2;
+            var kol = 1;
+
+            while (power.ToString().Length - 3 < delta)
+            {
+                Console.WriteLine(power);
+
+                if (kol % 2 == 1)
+                    power -= Power(x, i) / i;
+                else
+                    power += Power(x, i) / i;
+
+                i++;
+                kol++;
+                var progress = (double)(power.ToString().Length - 3) / delta * 100;
+                Console.WriteLine($"Прогресс: {progress}");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine(power);
+        }
+
+        static void Main(string[] args)
+        {
+            var task = new Task(() => Shit(2, 3));
+            task.Start();
+            Console.ReadKey();
+        }
+    }
+}
