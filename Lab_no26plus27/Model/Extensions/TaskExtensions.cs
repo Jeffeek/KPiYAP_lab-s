49c1b21b2065a07ae88dc676ae3014cfa81668ee
Mikhail Ilyushin <mishamine26@gmail.com>
2021-03-30 14:15:14 +0300

    Update icon width change

--- Lab_no26plus27/Model/Extensions/TaskExtensions.cs
+++ Lab_no26plus27/Model/Extensions/TaskExtensions.cs
@@ -9,9 +9,7 @@ namespace Lab_no26plus27.Model.Extensions
 {
     public static class TaskExtensions
     {
-        #pragma warning disable RECS0165 // Asynchronous methods should return a Task instead of void
         public static async void FireAndForgetSafeAsync(this Task task, IErrorHandler handler = null)
-            #pragma warning restore RECS0165 // Asynchronous methods should return a Task instead of void
         {
             try
             {
